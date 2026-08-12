43a00cf111cb1d4e93c04a1bf66fc5e1b7ceefc3
gogicha007 <gogicha@gmail.com>
2026-08-12 12:50:54 +0400

    chore: save the progress

--- src/roles/roles.controller.ts
+++ src/roles/roles.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Patch, UseGuards, Body, Param } from '@nestjs/common';
+import { Body, Controller, Delete, Param, Patch, Post, UseGuards } from '@nestjs/common';
 import { RolesService } from './roles.service';
 import { Roles } from '../auth/decorators/roles.decorator';
 import { RolesGuard } from '../auth/guards/roles.guard';
@@ -8,7 +8,7 @@ import { UpdateRoleDto } from './dto/update-role.dto';
 @UseGuards(JwtAuthGuard)
 @Controller('roles')
 export class RolesController {
-  constructor(private readonly rolesService: RolesService) {}
+  constructor(private readonly rolesService: RolesService) { }
 
   @Roles('OWNER', 'ADMIN')
   @UseGuards(RolesGuard)
@@ -16,4 +16,27 @@ export class RolesController {
   updatee(@Param('id') id: string, @Body() data: UpdateRoleDto) {
     return this.rolesService.update(id, data);
   }
+
+  @Roles('OWNER', 'ADMIN')
+  @Post(':id/permissions')
+  createPermission(@Param('id') roleId: string) {
+    console.log('create permission')
+  }
+  @Roles('OWNER', 'ADMIN')
+  @Patch(':id/permissions/:permissionId')
+  updatePermission(
+    @Param('id') roleId: string,
+    @Param('permissionId') permissionId: string) {
+    console.log('update permission ')
+  }
+  @Roles('OWNER', 'ADMIN')
+  @Delete(':id/permissions/:permissionId')
+  deletePermission(
+    @Param('id') roleId: string,
+    @Param('permissionId') permissionId: string
+  ) {
+    console.log('delete permission')
+  }
+
+
 }
